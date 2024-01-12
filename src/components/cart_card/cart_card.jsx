0200cf23eb9d7197d2c7a15a551e41aac3c5ab76
Tanti14 <santicostanti@gmail.com>
2024-01-12 20:15:09 -0300

    feat: Confirmacion al quitar prod del carrito

--- src/components/cart_card/cart_card.jsx
+++ src/components/cart_card/cart_card.jsx
@@ -9,9 +9,36 @@ import {
 import { formatPrice } from "../../utils/formatPrice";
 import { useDispatch } from "react-redux";
 import { addToCart, removeFromCart } from "../../redux/cart/cartSlice";
+import Swal from "sweetalert2";
 
 export const CartCard = ({ id, img, title, precio, quantity }) => {
   const dispatch = useDispatch();
+
+  const deleteCartItem = () => {
+    if (quantity === 1) {
+      Swal.fire({
+        title: "Eliminar producto del carrito?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#ffa500",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Eliminar!",
+        cancelButtonText: "Cancelar",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Producto eliminado",
+            icon: "success",
+            showConfirmButton: false,
+            timer: 2000,
+          });
+          dispatch(removeFromCart(id));
+        }
+      });
+    } else {
+      dispatch(removeFromCart(id));
+    }
+  };
   return (
     <CartCardContent>
       <img src={img} alt="" />
@@ -32,7 +59,7 @@ export const CartCard = ({ id, img, title, precio, quantity }) => {
           <CardBtn
             whileTap={{ scale: 0.95 }}
             whileHover={{ scale: 1.09 }}
-            onClick={() => dispatch(removeFromCart(id))}
+            onClick={() => deleteCartItem()}
           >
             -
           </CardBtn>
